5effdb115cc75717b682a37230768af781baefdc
Nicholas Staples <nicholas.staples@solution-tribute.com>
2016-01-20 13:15:49 +0000

    Added send_notification_email

--- client/notifications.py
+++ client/notifications.py
@@ -15,5 +15,17 @@ class NotificationsAPIClient(BaseAPIClient):
                 "notification": notification
             })
 
+    def send_email_notification(self, to, from_, subject, message):
+        notification = {}
+        notification.update({
+            "to": to,
+            "from": from_,
+            "subject": subject,
+            "message": message
+        })
+        return self.post(
+            '/notifications/email',
+            data={"notification": notification})
+
     def get_notification_by_id(self, id):
         return self.get('/notifications/{}'.format(id))
