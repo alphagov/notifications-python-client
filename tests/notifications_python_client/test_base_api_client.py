0063ce9db838e45ec3538c0573f956e9c15fce6c
Chris Hill-Scott <me@quis.cc>
2016-09-28 10:50:23 +0100

    Test any character can be used as key separator
    Hyphen might not be the best character to use, so we should make sure we
    can change it later.

--- tests/notifications_python_client/test_base_api_client.py
+++ tests/notifications_python_client/test_base_api_client.py
@@ -20,6 +20,15 @@ import requests
         )
     ),
     BaseAPIClient(
+        api_key=(
+            'name_of_key'
+            '😬'
+            'c745a8d8-b48a-4b0d-96e5-dbea0165ebd1'  # service ID
+            '😬'
+            '8b3aa916-ec82-434e-b0c5-d5d9b371d6a3'  # secret
+        )
+    ),
+    BaseAPIClient(
         service_id='c745a8d8-b48a-4b0d-96e5-dbea0165ebd1',
         api_key=(
             'name_of_key'
