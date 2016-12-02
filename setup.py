bb07a2b5b83fd2a4635628f4860c4d035aa8c8d7
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2016-12-02 15:26:09 +0000

    enhance setup.py to support `setup.py test` and add metadata
    `python setup.py test` now works - even if you're in a completely
    empty python environment, it'll download pytest and run all the tests.
    if we wish to add arguments, they should be put in setup.cfg
    
    also i added some metadata - some keyword that people might search for,
    and some classifiers to flag it as MIT and python3.4 only.

--- setup.py
+++ setup.py
@@ -7,17 +7,22 @@ import pip.download
 from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
-
+# can't just import notifications_python_client.version as requirements may not be installed yet and imports will fail
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('notifications_python_client/version.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = list(parse_requirements('requirements.txt',
-                                       session=pip.download.PipSession()))
+def get_reqs(requirement_file):
+    requirements = list(parse_requirements(requirement_file, session=pip.download.PipSession()))
+    return [str(r.req) for r in requirements]
 
-install_requires = [str(r.req) for r in requirements]
+install_requires = get_reqs('requirements.txt')
+tests_require = get_reqs('requirements_for_test.txt')
+# for running pytest as `python setup.py test`, see
+# http://doc.pytest.org/en/latest/goodpractices.html#integrating-with-setuptools-python-setup-py-test-pytest-runner
+setup_requires = ['pytest-runner']
 
 setup(
     name='notifications-python-client',
@@ -27,7 +32,19 @@ setup(
     author='Government Digital Service',
     description='Python API client for GOV.UK Notify.',
     long_description=__doc__,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        # TODO: Get tests running for other versions of python
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='gds govuk notify',
+
     packages=find_packages(),
     include_package_data=True,
-    install_requires=install_requires
+
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
 )
