07937441d1121e028907141c56695143071b831c
Samuel Williams <samuelhwilliams@gmail.com>
2022-09-26 10:20:36 +0100

    Add support for new doc download features
    Pass through `confirm_email_before_download` and `retention_period`
    parameters via `prepare_upload` calls so that clients can start to
    explicitly opt-in or opt-out of the new security features.
    
    Bump client to v6.4.0

--- notifications_python_client/__init__.py
+++ notifications_python_client/__init__.py
@@ -7,7 +7,7 @@
 #
 # -- http://semver.org/
 
-__version__ = '6.3.0'
+__version__ = '6.4.0'
 
 from notifications_python_client.errors import (  # noqa
     REQUEST_ERROR_MESSAGE,
--- notifications_python_client/utils.py
+++ notifications_python_client/utils.py
@@ -3,13 +3,17 @@ import base64
 DOCUMENT_UPLOAD_SIZE_LIMIT = 2 * 1024 * 1024
 
 
-def prepare_upload(f, is_csv=False):
+def prepare_upload(f, is_csv=False, confirm_email_before_download=None, retention_period=None):
     contents = f.read()
 
     if len(contents) > DOCUMENT_UPLOAD_SIZE_LIMIT:
         raise ValueError('File is larger than 2MB')
 
-    return {
+    file_data = {
         'file': base64.b64encode(contents).decode('ascii'),
         'is_csv': is_csv,
+        'confirm_email_before_download': confirm_email_before_download,
+        'retention_period': retention_period
     }
+
+    return file_data
