e6150e73abde02c32c1c954e9d522016b208747f
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-07-17 11:28:04 +0100

    remove deprecated setup.py functions
    using setup.py as a task runner is deprecated[^1]
    
    these paths to run the unit and integration tests aren't actually used
    anywhere within our own code - we just call the tests directly through
    pytest etc.
    
    [^1]: https://packaging.python.org/en/latest/discussions/setup-py-deprecated/#is-setup-py-deprecated

--- setup.py
+++ setup.py
@@ -8,31 +8,12 @@ import ast
 import re
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as TestCommand
 
 # can't just import notifications_python_client.version as requirements may not be installed yet and imports will fail
 _version_re = re.compile(r"__version__\s+=\s+(.*)")
 
 with open("notifications_python_client/__init__.py", "rb") as f:
     version = str(ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1)))
-
-
-class IntegrationTestCommand(TestCommand):
-    user_options = []
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ""
-
-    def run_tests(self):
-        import shlex
-        import sys
-
-        import pytest
-
-        errno = pytest.main(shlex.split(self.pytest_args) + ["integration_test/integration_tests.py"])
-        sys.exit(errno)
-
 
 setup(
     name="notifications-python-client",
@@ -63,8 +44,4 @@ setup(
         "PyJWT>=1.5.1",
         "docopt>=0.3.0",
     ],
-    # for running pytest as `python setup.py test`, see
-    # http://doc.pytest.org/en/latest/goodpractices.html#integrating-with-setuptools-python-setup-py-test-pytest-runner
-    setup_requires=["pytest-runner"],
-    cmdclass={"integration_test": IntegrationTestCommand},
 )
